f3b79c2f78a2e32a1e308d7e92d7c7f1a211440a
AdiatsaPutra <santikosetyo77@gmail.com>
2021-06-04 21:08:51 +0700

    Add edit still bug picturePath

--- resources/views/food/edit.blade.php
+++ resources/views/food/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Users &raquo; {{ $user->name }} &raquo; Update
+            Food &raquo; {{ $food->name }} &raquo; Edit
         </h2>
     </x-slot>
 
@@ -12,93 +12,74 @@
                     <div class="mb-5" role="alert">
                         <div class="font-bold bg-red-500 text-white rounded px-6 py-4">
                             Terjadi kesalahan
-                        </div>
-                        <div class="font-bold bg-red-500 text-white rounded px-6 py-4">
                             @foreach ($errors->all() as $error)
-                                <li>{{$error}}</li>
+                            <li>{{$error}}</li>
                             @endforeach
                         </div>
                     </div>
                 </div>
             @endif
-            <form action="{{ route('users.update', $user->id ) }}" method="post" enctype="multipart/form-data">
+            <form action="{{ route('food.update', $food->id) }}" method="post" enctype="multipart/form-data">
                 @csrf
                 @method('put')
                 <div class="col-span-6 sm:col-span-3 my-5">
-                    <label for="name" class="block text-lg font-medium text-gray-700">First name</label>
-                    <input  value="{{ old('name') ?? $user->name }}" type="text" name="name" id="name" placeholder="Masukkan Nama" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
-                </div>
-                <div class="col-span-6 sm:col-span-3 my-5">
-                    <label for="email" class="block text-lg font-medium text-gray-700">Email</label>
-                    <input  value="{{ old('email') ?? $user->email }}" type="email" name="email" id="email" placeholder="Masukkan Email" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
-                </div>
-                <div class="col-span-6 sm:col-span-3 my-5">
-                    <label for="foto" class="block text-lg font-medium text-gray-700">Foto</label>
-                    <input  value="{{ old('foto') }}" type="file" name="profile_photo_path" id="foto" placeholder="Foto" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                    <label for="name" class="block text-lg font-medium text-gray-700">Nama</label>
+                    <input value="{{ old('name') ?? $food->name }}" type="text" name="name" id="name" placeholder="Masukkan Nama" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
                 </div>
                 <div class="flex flex-wrap -mx-3 mb-6">
                     <div class="w-full px-3">
-                        <label class="block text-lg font-medium text-gray-700">
-                            Password
+                        <label class="block text-lg font-medium text-gray-700" for="grid-last-name">
+                            Foto
                         </label>
-                        <input value="{{ old('password') }}" name="password" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" id="grid-last-name" type="password" placeholder="User Password Confirmation">
+                        <input value="{{ old('picturePath') ?? $food->picturePath }}" name="picturePath" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" id="grid-last-name" type="hidden" placeholder="Food Image">
+                        <input name="picturePath" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" id="grid-last-name" type="file" placeholder="Food Image">
                     </div>
                 </div>
                 <div class="flex flex-wrap -mx-3 mb-6">
                     <div class="w-full px-3">
-                        <label class="block text-lg font-medium text-gray-700">
-                            Konfirmasi Password
+                        <label class="block text-lg font-medium text-gray-700" for="grid-last-name">
+                            Deskripsi
                         </label>
-                        <input value="{{ old('password_confirmation') }}" name="password_confirmation" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" id="grid-last-name" type="password" placeholder="User Password Confirmation">
+                        <input value="{{ old('description') ?? $food->description }}" name="description" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" type="text" placeholder="Deskripsi">
                     </div>
                 </div>
                 <div class="flex flex-wrap -mx-3 mb-6">
                     <div class="w-full px-3">
-                        <label class="block text-lg font-medium text-gray-700">
-                            Alamat
+                        <label class="block text-lg font-medium text-gray-700" for="grid-last-name">
+                            Bahan
                         </label>
-                        <input value="{{ old('address') ?? $user->address }}" name="address" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" id="grid-last-name" type="text" placeholder="User Address">
+                        <input value="{{ old('ingredients') ?? $food->ingredients }}" name="ingredients" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" type="text" placeholder="Bahan">
+                        <p class="text-gray-600 text-xs italic">Dipisahkan dengan koma, contoh: Bawang Merah, Bawang Paprika, Bawang Bombai.</p>
                     </div>
                 </div>
                 <div class="flex flex-wrap -mx-3 mb-6">
                     <div class="w-full px-3">
-                        <label class="block text-lg font-medium text-gray-700">
-                            Roles
+                        <label class="block text-lg font-medium text-gray-700" for="grid-last-name">
+                            Harga
                         </label>
-                        <select name="roles" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" id="grid-last-name">
-                            <option value="{{ old('roles') ?? $user->roles }}">{{ old('roles') ?? $user->roles }}</option>
-                            <option value="USER">User</option>
-                            <option value="ADMIN">Admin</option>
-                        </select>
+                        <input value="{{ old('price') ?? $food->price }}" name="price" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" type="number" placeholder="Harga">
                     </div>
                 </div>
                 <div class="flex flex-wrap -mx-3 mb-6">
                     <div class="w-full px-3">
-                        <label class="block text-lg font-medium text-gray-700">
-                            House Number
+                        <label class="block text-lg font-medium text-gray-700" for="grid-last-name">
+                            Rating
                         </label>
-                        <input value="{{ old('houseNumber') ?? $user->houseNumber }}" name="houseNumber" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" id="grid-last-name" type="text" placeholder="User House Number">
+                        <input value="{{ old('rate') ?? $food->rate }}" name="rate" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" type="number", step="0.01" max="5" placeholder="Rating">
                     </div>
                 </div>
                 <div class="flex flex-wrap -mx-3 mb-6">
                     <div class="w-full px-3">
-                        <label class="block text-lg font-medium text-gray-700">
-                            Phone Number
+                        <label class="block text-lg font-medium text-gray-700" for="grid-last-name">
+                            Tipe
                         </label>
-                        <input value="{{ old('phoneNumber') ?? $user->phoneNumber }}" name="phoneNumber" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" id="grid-last-name" type="text" placeholder="User Phone Number">
-                    </div>
-                </div>
-                <div class="flex flex-wrap -mx-3 mb-6">
-                    <div class="w-full px-3">
-                        <label class="block text-lg font-medium text-gray-700">
-                            City
-                        </label>
-                        <input value="{{ old('city') ?? $user->city }}" name="city" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" id="grid-last-name" type="text" placeholder="User City">
+                        <input value="{{ old('type') ?? $food->type }}" name="type" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" type="text" placeholder="Tipe Makanan">
+                        <p class="text-gray-600 text-xs italic">Dipisahkan dengan koma, contoh: recommended, popular, new_food</p>
                     </div>
                 </div>
                 <div class="text-right">
                     <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                      Update
+                      Simpan
                     </button>
                 </div>
             </form>
