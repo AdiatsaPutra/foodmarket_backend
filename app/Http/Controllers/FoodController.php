f91e1997a59a72601c125eb07596247c8d2d25f1
AdiatsaPutra <santikosetyo77@gmail.com>
2021-06-04 21:10:45 +0700

    Add delete

--- app/Http/Controllers/FoodController.php
+++ app/Http/Controllers/FoodController.php
@@ -95,6 +95,8 @@ class FoodController extends Controller
      */
     public function destroy(Food $food)
     {
-        //
+        $food->delete();
+
+        return redirect()->route('food.index');
     }
 }
